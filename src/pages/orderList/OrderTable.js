6c623a5b854f42570ee447080c2a3bdee59392c4
sooin <csi44029674@gmail.com>
2024-04-13 13:42:48 +0900

    A : 하단 페이지 버튼

--- src/pages/orderList/OrderTable.js
+++ src/pages/orderList/OrderTable.js
@@ -7,14 +7,34 @@ const OrderTable = () => {
         { id: 2, menu: '왕돈까스', date: '2024-03-29 18:00', status: '신규', price: 25000 },
         { id: 3, menu: '김치우동', date: '2024-03-29 18:00', status: '준비중', price: 15000 },
         { id: 4, menu: '왕돈까스, 김치우동', date: '2024-03-29 18:00', status: '신규', price: 15000 },
+         { id: 5, menu: '왕돈까스, 김치우동', date: '2024-03-29 18:00', status: '신규', price: 15000 },
+        { id: 6, menu: '왕돈까스', date: '2024-03-29 18:00', status: '신규', price: 25000 },
+        { id: 7, menu: '김치우동', date: '2024-03-29 18:00', status: '준비중', price: 15000 },
+        { id: 8, menu: '왕돈까스, 김치우동', date: '2024-03-29 18:00', status: '신규', price: 15000 },
+         { id: 9, menu: '왕돈까스, 김치우동', date: '2024-03-29 18:00', status: '신규', price: 15000 },
+        { id: 10, menu: '왕돈까스', date: '2024-03-29 18:00', status: '신규', price: 25000 },
+        { id: 11, menu: '김치우동', date: '2024-03-29 18:00', status: '준비중', price: 15000 },
+        { id: 12, menu: '왕돈까스, 김치우동', date: '2024-03-29 18:00', status: '신규', price: 15000 },
+         { id: 13, menu: '왕돈까스, 김치우동', date: '2024-03-29 18:00', status: '신규', price: 15000 },
+        { id: 14, menu: '왕돈까스', date: '2024-03-29 18:00', status: '신규', price: 25000 },
+        { id: 15, menu: '김치우동', date: '2024-03-29 18:00', status: '준비중', price: 15000 },
+        { id: 16, menu: '왕돈까스, 김치우동', date: '2024-03-29 18:00', status: '신규', price: 15000 },
     ];
 
-    const [pageNation, setPageNation] = useState(1);
-    
+    const itemsPerPage = 10;
+    const [currentPage, setCurrentPage] = useState(1);
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentOrders = orders.slice(indexOfFirstItem,indexOfLastItem);
+
+    const handleClick = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
     return(
-        <div class="pt-6 h-screen">
-            <table class="w-full">
-                <thead class="bg-gray-50 border-b-2 border-gray-800 ">
+        <div class="pt-6 h-screen flex flex-col items-center">
+            <table class="w-full bg-gray-800 text-white">
+                <thead class="border-b-2 border-gray-800 ">
                     <tr class="">
                         <th class="w-12 p-3 text-sm font-semibold tracking-wide text-left">번호</th>
                         <th class="w-40 p-3 text-sm font-semibold tracking-wide text-left">메뉴</th>
@@ -23,27 +43,31 @@ const OrderTable = () => {
                         <th class="w-14 p-3 text-sm font-semibold tracking-wide text-left">상태</th>
                     </tr>
                 </thead>
-                <tbody class="bg-white">
-                    {orders.map(order => (
-                        <tr key = {order.id}>
-                            <td class="p-3 text-sm font-bold">{order.id}</td>
-                            <td class="p-3 text-sm text-gray-700">{order.menu}</td>
-                            <td class="p-3 text-sm text-gray-700">{order.date}</td>
-                            <td class="p-3 text-sm text-gray-700">{order.price}</td>
-                            <td class="p-3 text-sm text-gray-700">
-                                <span class="p-1.5 text-xs font-medium uppercase tracking-wider text-yellow-800 bg-red-200 rounded-lg bg-opacity-40">{order.status}</span>
+                <tbody className="">
+                    {currentOrders.map((order) => (
+                        <tr key={order.id}>
+                            <td className="p-3 text-sm font-bold">{order.id}</td>
+                            <td className="p-3 text-sm text-gray-700">{order.menu}</td>
+                            <td className="p-3 text-sm text-gray-700">{order.date}</td>
+                            <td className="p-3 text-sm text-gray-700">{order.price}</td>
+                            <td className="p-3 text-sm text-gray-700">
+                                <span className="p-1.5 text-xs font-medium uppercase tracking-wider text-yellow-800 bg-red-200 rounded-lg bg-opacity-40">{order.status}</span>
                             </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-            {<div>
-                {[1,2,3,4,5].map(buttonNumber => (
-                    <button className="text-4xl text-white font-bold" key = {buttonNumber} onClick={()=> setPageNation(buttonNumber)}>
-                        {buttonNumber}
+            <div className="flex mt-4">
+                {[...Array(Math.ceil(orders.length / itemsPerPage)).keys()].map((number) => (
+                    <button
+                        className={`text-lg font-semibold mx-2 px-4 py-2 rounded-lg focus:outline-none ${currentPage === number + 1 ? 'bg-gray-800 text-white' : 'bg-gray-200 text-white'}`}
+                        key={number + 1}
+                        onClick={() => handleClick(number + 1)}
+                    >
+                        {number + 1}
                     </button>
                 ))}
-            </div> }
+            </div>
         </div>
     );
 };
