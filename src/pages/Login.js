19f039c3a5d78bae6a1dda3222af52ef10b5f99b
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-04-18 15:12:58 +0900

    ADD : 아이디 비번 유효성 검사

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,14 +5,24 @@ export default function Login(){
 
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
+  const [isRemember, setIsRemember] = useState(true);
+
+  const hasCredential = () => {
+    if (id.length <=0 && password.length <= 0) {
+        alert("아이디와 비밀번호를 입력해주세요");
+    }else if (id.length <= 0){
+      alert("아이디를 입력해주세요");
+    }else if (password.length <= 0) {
+      alert("비밀번호를 입력해주세요");
+    }
+  }
 
   return(
       <div className="flex items-center justify-center h-screen bg-gray-800">
         <div className="bg-black p-8 rounded-lg max-w-3xl w-full">
-          <h1 class="text-center text-white text-2xl font-bold mb-6">nice to menu</h1>
-          <h2 class="text-center text-white font-bold mb-6">로그인</h2>
+          <h1 className="text-center text-white text-2xl font-bold mb-6">nice to menu</h1>
+          <h2 className="text-center text-white font-bold mb-6">로그인</h2>
 
-          <form>
             <div className ="mb-6">
               <div className ="mt-4 relative rounded-md shadow-sm">
                 <input type="text"
@@ -38,7 +48,12 @@ export default function Login(){
             <div className="justify-between mb-6 flex items-center">
               <div className="form-control w-28">
                 <label className="label cursor-pointer">
-                  <input type="checkbox" defaultChecked className="checkbox checkbox-primary"/>
+                  <input type="checkbox"
+                         defaultChecked
+                         className="checkbox checkbox-primary"
+                         checked={isRemember}
+                         onChange = {(e) => setIsRemember(e.target.checked)}
+                  />
                   <span className="label-text text-white">아이디 저장</span>
                 </label>
               </div>
@@ -49,11 +64,10 @@ export default function Login(){
 
             <div>
               <button type="submit"
-                      style={{backgroundColor : "#3e538c"}}
+                      onClick={() => hasCredential()}
                       className="w-full flex justify-center py-4 px-4 border-transparent rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
               >로그인</button>
             </div>
-          </form>
         </div>
       </div>
     )
