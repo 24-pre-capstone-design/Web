739ea139123e8589da3d4fbdc0c0196e4950c8b5
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-04-19 15:00:24 +0900

    FIX : 사이드바 토클 리팩토링

--- src/components/topnav.js
+++ src/components/topnav.js
@@ -6,9 +6,13 @@ import * as Icon from 'react-feather'
 import {AiOutlineSetting, FaUser, IoMdLogOut} from '../assets/icons/vander'
 import 'simplebar-react/dist/simplebar.min.css';
 
-export default function Topnav(){
+export default function Topnav({toggle, setToggle}){
 
-    const [toggle, setToggle] = useState(false);
+    const [adminToggle, setAdminToggle] = useState(false);
+
+    const toggleHandler = () => {
+        setToggle(!toggle);
+    }
 
     return(
         <>
@@ -18,18 +22,18 @@ export default function Topnav(){
 
                         <Link id="close-sidebar"
                               className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[20px] text-center bg-gray-50 hover:bg-gray-100 border border-gray-400 text-slate-900 rounded-full">
-                            <Icon.Menu  className="h-4 w-4"/>
+                            <Icon.Menu  className="h-4 w-4" onClick={toggleHandler}/>
                         </Link>
 
                     </div>
 
                     <ul className="list-none mb-0 space-x-1">
                         <li className="dropdown inline-block relative">
-                            <button className="dropdown-toggle items-center" type="button" onClick={() => setToggle(!toggle)}>
+                            <button className="dropdown-toggle items-center" type="button" onClick={() => setAdminToggle(!adminToggle)}>
                                 <span className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[20px] text-center bg-gray-50 hover:bg-gray-100 border border-gray-100 text-slate-900 rounded-full"><FaUser className="icon" /></span>
                                 <span className="font-semibold text-[16px] text-lg text-white ms-1 px-2 sm:inline-block">관리자님</span>
                             </button>
-                            <div className={`dropdown-menu absolute end-0 m-0 mt-4 z-10 w-44 rounded-md overflow-hidden bg-black border border-gray-400 shadow ${toggle ? '' : 'hidden'}`} >
+                            <div className={`dropdown-menu absolute end-0 m-0 mt-4 z-10 w-44 rounded-md overflow-hidden bg-black border border-gray-400 shadow ${adminToggle ? '' : 'hidden'}`} >
                                 <ul className="py-2 text-start">
                                     <li>
                                         <Link to="/setting" className="flex items-center font-medium py-1 px-4 text-white hover:text-indigo-600"><AiOutlineSetting className="me-2"/>관리자 설정</Link>
