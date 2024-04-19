3aaab3bf26e34a1a32d64be05e543994b71953d6
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-04-19 12:03:24 +0900

    MOD : info alert 배경색 어둡게

--- src/components/alert/Info.js
+++ src/components/alert/Info.js
@@ -1,6 +1,6 @@
 export default function Info({message}){
     return (
-        <div role="alert" className="alert shake">
+        <div role="alert" className="alert shake bg-black border border-gray-800 text-white">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
             <span>{message}</span>
         </div>
