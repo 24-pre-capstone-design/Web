9f3ffe6af62ec5c17fe32e685effbec5f1437c4f
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-04-23 10:45:03 +0900

    ADD : sidebar topnav 연결

--- src/pages/MenuList.js
+++ src/pages/MenuList.js
@@ -1,6 +1,20 @@
+import Sidebar from "../components/sidebar";
+import Topnav from "../components/topnav";
+import React, {useState} from "react";
+
 export default function MenuList() {
+
+    const [toggle, setToggle] = useState(true);
+
     return (
         <>
+            <div className={`page-wrapper bg-black ${toggle ? "toggled" : ""}`}>
+                <Sidebar/>
+                <main className="page-content h-screen">
+                    <Topnav toggle={toggle} setToggle={setToggle}/>
+
+                </main>
+            </div>
         </>
     )
 }
