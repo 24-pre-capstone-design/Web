7156ea3c30f4be6df936e22e9b785a556110464d
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-04-23 10:44:02 +0900

    ADD : sidebar menulist 연결

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Route, Routes} from 'react-router-dom';
 import Home from "./pages/Home";
 import OrderPage from "./pages/OrderPage";
 import Login from "./pages/Login";
+import MenuList from "./pages/MenuList";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
               <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
               <Route path="/order" element={<OrderPage/>}/>
+              <Route path="/menu" element={<MenuList />} />
           </Routes>
       </>
   );
