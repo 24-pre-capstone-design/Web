8026a3b09a0355700f03ceeae697195d81c9f340
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-04-23 11:01:02 +0900

    ADD : 메뉴 더미데이터 추가

--- src/data/data.js
+++ src/data/data.js
@@ -1,1 +1,84 @@
 export const test = "test data";
+
+export const menuItem = [
+    {
+        id: 1,
+        name: "짜장면",
+        price: 6000,
+        foodCategory: "면류",
+        status: "판매중",
+        pictureURL: ""
+    },
+    {
+        id: 2,
+        name: "짬뽕",
+        price: 8000,
+        foodCategory: "면류",
+        status: "판매중",
+        pictureURL: ""
+    },
+    {
+        id: 3,
+        name: "볶음밥",
+        price: 7000,
+        foodCategory: "밥류",
+        status: "판매중",
+        pictureURL: ""
+    },
+    {
+        id: 4,
+        name: "탕수육",
+        price: 15000,
+        foodCategory: "특식",
+        status: "판매중",
+        pictureURL: ""
+    },
+    {
+        id: 5,
+        name: "깐풍기",
+        price: 13000,
+        foodCategory: "특식",
+        status: "판매중",
+        pictureURL: ""
+    },
+    {
+        id: 6,
+        name: "짜장밥",
+        price: 8000,
+        foodCategory: "밥류",
+        status: "판매중",
+        pictureURL: ""
+    },
+    {
+        id: 7,
+        name: "짬뽕밥",
+        price: 9000,
+        foodCategory: "밥류",
+        status: "판매중",
+        pictureURL: ""
+    },
+    {
+        id: 8,
+        name: "간짜장",
+        price: 7000,
+        foodCategory: "면류",
+        status: "판매중",
+        pictureURL: ""
+    },
+    {
+        id: 9,
+        name: "닭볶음탕",
+        price: 18000,
+        foodCategory: "특식",
+        status: "판매중",
+        pictureURL: ""
+    },
+    {
+        id: 10,
+        name: "유산슬",
+        price: 16000,
+        foodCategory: "특식",
+        status: "판매중",
+        pictureURL: ""
+    }
+];
