38fd4864ef72e0d4102ec5f2c05a0da0f18f6fdf
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-05-14 00:35:26 +0900

    ADD : access token 여부에 따른 접근 제한

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React from "react";
-import {Route, Routes} from 'react-router-dom';
+import {Navigate, Route, Routes} from 'react-router-dom';
 
 import Home from "./pages/Home";
 import OrderPage from "./pages/OrderPage";
 import Login from "./pages/Login";
 import MenuList from "./pages/MenuList";
 import FindPassword from "./pages/FindPassword";
+import {useCookies} from "react-cookie";
 
 export default function App() {
+
+    const accessToken = useCookies(['accessToken']);
 
   return (
       <>
           <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/order" element={<OrderPage/>}/>
-              <Route path="/menu" element={<MenuList />} />
-              <Route path="/find-password" element={<FindPassword />} />
+              {
+                    accessToken[0].accessToken ?
+                        <>
+                            <Route path="/home" element={<Home />} />
+                            <Route path="/order" element={<OrderPage/>}/>
+                            <Route path="/menu" element={<MenuList />} />
+                        </>
+                        :
+                        <>
+                            <Route path="/" element={<Login />} />
+                            <Route path="/find-password" element={<FindPassword />} />
+                        </>
+              }
           </Routes>
       </>
   );
