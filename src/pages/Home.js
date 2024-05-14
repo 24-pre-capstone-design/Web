1983bda22b90c8093afd04b0b47196b728e076a0
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-05-14 11:38:37 +0900

    ADD : 캐러샐 적용을 위한 react-slick 라이브러리 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -165,7 +165,6 @@ export default function Home() {
                                 </table>
                             </div>
 
-
                         </div>
                     </section>
 
