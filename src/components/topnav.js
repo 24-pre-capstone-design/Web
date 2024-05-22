34d6a28edea9b54078b66a562f11b50a870d702a
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-05-22 15:59:20 +0900

    DEL : 상단바 주석 제거

--- src/components/topnav.js
+++ src/components/topnav.js
@@ -65,41 +65,6 @@ export default function Topnav({toggle, setToggle}){
                     <a className="btn btn-neutral" onClick={() => {logout()}}>log out</a>
                 </div>
             </div>
-
-            {/*<div className="top-header">*/}
-            {/*    <div className="header-bar flex justify-between">*/}
-            {/*        <div className="flex items-center space-x-1">*/}
-
-            {/*            <Link id="close-sidebar"*/}
-            {/*                  className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[20px] text-center bg-gray-50 hover:bg-gray-100 border border-gray-400 text-slate-900 rounded-full">*/}
-            {/*                <Icon.Menu  className="h-4 w-4" onClick={toggleHandler}/>*/}
-            {/*            </Link>*/}
-
-            {/*        </div>*/}
-
-            {/*        <ul className="list-none mb-0 space-x-1">*/}
-            {/*            <li className="dropdown inline-block relative">*/}
-            {/*                <button className="dropdown-toggle items-center" type="button" onClick={() => setAdminToggle(!adminToggle)}>*/}
-            {/*                    <span className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[20px] text-center bg-gray-50 hover:bg-gray-100 border border-gray-100 text-slate-900 rounded-full"><FaUser className="icon" /></span>*/}
-            {/*                    <span className="font-semibold text-[16px] text-lg text-white ms-1 px-2 sm:inline-block">관리자님</span>*/}
-            {/*                </button>*/}
-            {/*                <div className={`dropdown-menu absolute end-0 m-0 mt-4 z-10 w-44 rounded-md overflow-hidden bg-black border border-gray-400 shadow ${adminToggle ? '' : 'hidden'}`} >*/}
-            {/*                    <ul className="py-2 text-start">*/}
-            {/*                        <li>*/}
-            {/*                            <Link to="/setting" className="flex items-center font-medium py-1 px-4 text-white hover:text-indigo-600"><AiOutlineSetting className="me-2"/>관리자 설정</Link>*/}
-            {/*                        </li>*/}
-            {/*                        <li>*/}
-            {/*                            <Link*/}
-            {/*                                className="flex items-center font-medium py-1 px-3 text-white hover:text-indigo-600"*/}
-            {/*                                onClick={() => {logout();}}>*/}
-            {/*                                <IoMdLogOut className="me-2"/>로그아웃</Link>*/}
-            {/*                        </li>*/}
-            {/*                    </ul>*/}
-            {/*                </div>*/}
-            {/*            </li>*/}
-            {/*        </ul>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </>
     )
 }
