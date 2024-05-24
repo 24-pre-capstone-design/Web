6479e178ad5ab5792b05e5d29f86ddd77eafed0b
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-05-24 21:05:01 +0900

    MOD : 메뉴 관리 컴포넌트 분리

--- src/pages/MenuList.js
+++ src/pages/MenuList.js
@@ -8,13 +8,13 @@ import NewCategory from "../components/modal/NewCategory";
 import Footer from "../components/widgets/Footer";
 import PageTitle from "../components/PageManager/PageTitle";
 import DeleteCheckedItem from "../components/modal/DeleteCheckedItem";
+import MenuCards from "../components/widgets/MenuCards";
 
 export default function MenuList() {
 
     const [toggle, setToggle] = useState(true);
     const [page, setPage] = useState(1);
     const [size, setSize] = useState(8);
-    const [menuItem, setMenuItem] = useState([]);
     const [checkboxShow, setCheckboxShow] = useState(false);
     const [selectedCategory, setSelectedCategory] = useState('');
 
@@ -24,10 +24,6 @@ export default function MenuList() {
             setSelectedCategory('');
         }
     },[selectedCategory]);
-
-    useEffect(() => {
-        setMenuItem(menuData);
-    }, []);
 
     return (
         <>
@@ -39,40 +35,7 @@ export default function MenuList() {
                     <PageTitle title={"메뉴 관리"} checkboxShow={checkboxShow} setCheckboxShow={setCheckboxShow} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
 
                     <section className="relative top-6 mt-8 mb-8 mx-10">
-                        <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 max-w-[1440px]">
-
-                            {
-                                menuItem && menuItem.map((item, index) => {
-                                    const modalId = `updateModal-${item.id}`;
-                                    return (
-                                        <>
-                                            <div className="card h-auto bg-base-100 shadow-xl my-3 mx-2" key={item.id}>
-                                                <figure><img src={item.pictureURL} alt=""/></figure>
-                                                <div className="card-body">
-                                                    <h2 className="card-title justify-between">
-                                                        <div>
-                                                            {item.name}
-                                                            <div className="badge badge-accent badge-outline align-middle mb-1 mx-2">{item.foodCategory}</div>
-                                                            <div className="badge badge-warning badge-outline align-middle mb-1">{item.status}</div>
-                                                        </div>
-                                                        <input type="checkbox" className={`checkbox checkbox-error ${checkboxShow ? '' : 'hidden'}`} />
-                                                    </h2>
-                                                    <p>{item.price}원</p>
-                                                    <div className="card-actions justify-end">
-                                                        <button className="btn btn-block" onClick={() => document.getElementById(modalId).showModal()}>메뉴 수정하기</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <dialog id={modalId} className="modal" key={item.id}>
-                                                <EditMenu item={item}/>
-                                            </dialog>
-                                        </>
-                                    )
-                                })
-                            }
-
-                        </div>
+                        <MenuCards checkboxShow={checkboxShow} />
                         <Pagenation page={page}/>
                     </section>
 
@@ -84,9 +47,7 @@ export default function MenuList() {
                         <EditMenu />
                     </dialog>
 
-                    <dialog id="newCategoryModal" className="modal">
-                        <NewCategory />
-                    </dialog>
+                    <NewCategory />
 
                     <div className="relative top-20">
                         <Footer/>
