02428670b13362ace42d094ef650ca67af7fb6a3
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-05-24 18:13:54 +0900

    MOD : 직원 관리 컴포넌트 분리

--- src/pages/EmployeeList.js
+++ src/pages/EmployeeList.js
@@ -1,13 +1,12 @@
 import React, {useState} from "react"
 import Sidebar from "../components/sidebar";
 import Topnav from "../components/topnav";
-import {employeeData, employeeStatus} from "../data/data"
 import Pagenation from "../components/widgets/Pagenation";
 import Footer from "../components/Footer";
 import EditEmployee from "../components/modal/EditEmployee";
 import Info from "../components/alert/Info";
-import EmployeeManager from "../components/widgets/PageManager/EmployeeManager";
 import PageTitle from "../components/widgets/PageTitle";
+import EmployeeTable from "../components/table/EmployeeTable";
 
 export default function EmployeeList(){
 
@@ -44,7 +43,7 @@ export default function EmployeeList(){
                         </div>
                     </div>
 
-                    <PageTitle title={"직원 관리"} />
+                    <PageTitle title={"직원 관리"} checkboxShow={checkboxShow} setCheckboxShow={setCheckboxShow}/>
 
                     <section className="relative top-6 mx-10 mt-8">
 
@@ -63,56 +62,7 @@ export default function EmployeeList(){
                             </select>
                         </div>
 
-                        <div className="overflow-x-auto p-2">
-                            <table className="table text-center mt-2">
-                                <thead>
-                                <tr className="h-11 border-t border-gray-400 text-gray-50 text-[14px]">
-                                    <th className="w-10"></th>
-                                    <th className="w-14">번호</th>
-                                    <th>이름</th>
-                                    <th>전화번호</th>
-                                    <th>근무요일</th>
-                                    <th>근무상태</th>
-                                    <th>직원 수정</th>
-                                </tr>
-                                </thead>
-                                <tbody className="text-white/85">
-                                {
-                                    employeeData.map((item, index) => {
-                                        return (
-                                            <tr key={index} className={`border-b border-gray-800 ${index%2===0 ? 'bg-gray-800/30' : ''}`}>
-                                                <td>
-                                                    <input type="checkbox" className={`checkbox checkbox-error mx-2 ${checkboxShow ? '' : 'hidden'}`} />
-                                                </td>
-                                                <td>{item.id}</td>
-                                                <td>{item.name}</td>
-                                                <td>
-                                                    {
-                                                        telVisibleIndex === index ? item.phoneNumber :
-                                                            <>
-                                                                <button
-                                                                    className="btn btn-xs btn-active"
-                                                                    onClick={()=>showTel(index)}>전화번호 보기</button>
-                                                            </>
-                                                    }
-                                                </td>
-                                                <td>{item.workDate}</td>
-                                                <td>{
-                                                    employeeStatus.map((status, index) => {
-                                                        if (status.value === item.status) {
-                                                            return status.label
-                                                        }
-                                                    })
-                                                }</td>
-                                                <td>
-                                                    <button className="btn btn-xs btn-success" onClick={()=>updateEmployee(item)}>수정</button>
-                                                </td>
-                                            </tr>
-                                    )})
-                                }
-                                </tbody>
-                            </table>
-                        </div>
+                        <EmployeeTable checkboxShow={checkboxShow} telVisibleIndex={telVisibleIndex} showTel={showTel} updateEmployee={updateEmployee}/>
                         <Pagenation />
                     </section>
 
