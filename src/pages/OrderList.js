34ed9bb8f6fee45daf8146d37c7fbca798bf183a
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-05-24 18:09:29 +0900

    MOD : 주문 관리 컴포넌트 분리

--- src/pages/OrderList.js
+++ src/pages/OrderList.js
@@ -1,19 +1,14 @@
-import React, {useEffect, useState} from "react"
+import React, {useState} from "react"
 import Sidebar from "../components/sidebar";
 import Topnav from "../components/topnav";
-import {orderData, orderHistoryStatus} from "../data/data"
 import Pagenation from "../components/widgets/Pagenation";
 import Footer from "../components/Footer";
 import PageTitle from "../components/widgets/PageTitle";
+import OrderTable from "../components/table/OrderTable";
 
 export default function OrderList(){
 
     const [toggle, setToggle] = useState(true);
-    const [orderItem, setOrderItem] = useState([]);
-
-      useEffect(() => {
-        setOrderItem(orderData)
-    }, []);
 
     return(
         <>
@@ -39,44 +34,7 @@ export default function OrderList(){
                             </select>
 
                         </div>
-                        <div className="overflow-x-auto p-2">
-                            <table className="table text-center mt-2">
-                                <thead>
-                                    <tr className="h-10 border-t border-gray-400 text-gray-50">
-                                        <th>번호</th>
-                                        <th>결제아이디</th>
-                                        <th>메뉴</th>
-                                        <th>수량</th>
-                                        <th>가격</th>
-                                        <th>상태</th>
-                                        <th>주문시각</th>
-                                    </tr>
-                                </thead>
-                                <tbody className="text-white/85">
-                                {
-                                    orderItem.map((item, index) => {
-                                        return (
-                                            <tr key={index} className={`border-b border-gray-800 ${index%2===0 ? 'bg-gray-800/30' : ''}`}>
-                                                <td>{item.id}</td>
-                                                <td>{item.paymentId}</td>
-                                                <td>{item.foodName}</td>
-                                                <td>{item.quantity}</td>
-                                                <td>{item.sumOfCost}</td>
-                                                <td>{
-                                                    orderHistoryStatus.map((status, index) => {
-                                                        if (status.value === item.orderHistoryStatus) {
-                                                            return status.label
-                                                        }
-                                                    })
-                                                }</td>
-                                                <td>{item.orderedAt}</td>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                                </tbody>
-                            </table>
-                        </div>
+                        <OrderTable />
                         <Pagenation />
                     </section>
 
